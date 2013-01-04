a65d632398258ebfeccd5f0095c7e6be113b5a95
Demarius Chrite <chrite.demarius@gmail.com>
2013-01-04 10:00:08 -0500

    ugh

---
+++ life.py
@@ -0,0 +1,71 @@
+# 1. Any live cell with fewer than two live neighbours dies
+# 2. Any live cell two or three live neighbours lives on to the next generation.
+# 3. Any live vell with more than three live neighbours dies, as if by overcrowding.
+# 4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction
+
+from random import randint
+
+class World:
+
+	def __init__(self, seed=None, height=10, width=10):
+		if seed == None:
+			self.height = height
+			self.width = width
+
+			self.table = [None] * width
+			for i in range(width):
+				self.table[i] = [None] * height
+				for x in range(height):
+					rand = randint(0, 2)
+					if rand == 0:
+						self.table[i][x] = rand
+					else:
+						self.table[i][x] = 1
+		else:
+			self.table = seed
+			self.width = len(seed)
+			self.height = len(seed[0])
+
+
+	def liveNeighbors(self, x, y):
+	
+		ret = 0
+		if x > 0:
+			ret += self.table[x-1][y]
+			if y > 0:
+				ret += self.table[x-1][y-1]
+
+			if y < self.height:
+				ret += self.table[x-1][y+1]
+
+		if x < self.width:
+			ret += self.table[x+1][y]
+
+			if y > 0:
+				ret += self.table[x+1][y-1]
+			if y < self.height:
+				ret += self.table[x+1][y+1]
+
+		if y > 0:
+			ret += self.table[x][y-1]
+		if y < self.height:
+			ret += self.table[x][y+1]
+		return ret
+
+	def deadNeighbors(self, x, y):
+	#Obviously not necessary but short enough to be worth writing anyway
+		return 9 - self.live(x, y)
+
+
+	def gen(self):
+		next_gen = self.table
+		for x in range(self.width):
+			for y in range(self.height):
+				neighbors = self.liveNeighbors(x, y)
+				if neighbors > 2: next_gen[x][y] = 0
+				if neighbors > 3: next_gen[x][y] = 0
+
+				if self.table[x][y] == 0:
+					if neighbors == 3: next_gen[x][y] == 1
+				if self.table[x][y] == 1:
+					if neighbors == 2 or neighbors == 2: next_gen[x][y] == 1

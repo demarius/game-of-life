8ad5d5e90a905a4fdd9d7ac95650dbd7c8547881
Demarius Chrite <chrite.demarius@gmail.com>
2013-01-04 10:02:39 -0500

    Update life.py

--- life.py
+++ life.py
@@ -1,7 +1,7 @@
-# 1. Any live cell with fewer than two live neighbours dies
+# 1. Any live cell with fewer than two live neighbours dies.
 # 2. Any live cell two or three live neighbours lives on to the next generation.
 # 3. Any live vell with more than three live neighbours dies, as if by overcrowding.
-# 4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction
+# 4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
 
 from random import randint
 
